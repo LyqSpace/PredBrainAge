12b3ffc173153b660573bf87564010e9a853bca4
LyqSerenity <root@lyq.me>
2018-01-26 13:59:30 +0800

    Finished offset div zoom sub

--- src/IL/InductiveLearning.py
+++ src/IL/InductiveLearning.py
@@ -1,3 +1,6 @@
+from scipy import ndimage
+import numpy as np
+
 from src.IL.Database import Database
 import src.utils as utils
 
@@ -6,8 +9,13 @@ class InductiveLearning:
 
     def __init__(self):
         self._database = Database()
-        self._template_data = None
-        self._template_am = None
+
+        self._divisor = 2
+        self._log_c = np.log(2 ** 3)
+        self._block_anchors = np.array([[0, 0.6], [0.4, 1]])
+        self._divide_level_limit = 4
+
+        self._zoom_rates = np.arange(0.7, 1.4, 0.1)
 
     def load_group(self):
         pass
@@ -24,20 +32,100 @@ class InductiveLearning:
 
             print('\tGroup Age: {0}.'.format(group_age))
 
-            _, self._template_data, _ = self._database.get_next_data_from_group()
+            _, template_data, _ = self._database.get_next_data_from_group()
+            template_am = utils.get_attention_map(template_data)
 
-            utils.show_3Ddata(self._template_data)
-
-            self._template_am = utils.get_attention_map(self._template_data)
+            # utils.show_3Ddata(template_am)
 
             while self._database.has_next_data_from_group():
 
-                data_name, data, age = self._database.get_next_data_from_group()
-                self._inductive_learning(data)
+                data_name, matched_data, age = self._database.get_next_data_from_group()
+                matched_am = utils.get_attention_map(matched_data)
+                self._induce(0, template_data, template_am, matched_data, matched_am)
 
         print('Done.')
 
-    def _inductive_learning(self, data):
+    def _induce(self, block_id, template_data, template_am, matched_data, matched_am):
+
+        divide_level = 0
+        if block_id > 0:
+            divide_level = np.floor(np.log(block_id) / self._log_c) + 1
+
+        if divide_level == self._divide_level_limit:
+            return
+
+        template_center = np.array(ndimage.measurements.center_of_mass(template_am))
+        matched_center = np.array(ndimage.measurements.center_of_mass(matched_am))
+
+        best_div = np.inf
+        best_zoom_rate = np.ones(3)
+
+        for zoom_rate0 in self._zoom_rates:
+            for zoom_rate1 in self._zoom_rates:
+                for zoom_rate2 in self._zoom_rates:
+
+                    zoomed_matched_am = ndimage.zoom(matched_am, [zoom_rate0, zoom_rate1, zoom_rate2])
+                    zoomed_matched_center = np.array([matched_center[0] * zoom_rate0,
+                                                      matched_center[1] * zoom_rate1,
+                                                      matched_center[2] * zoom_rate2])
+
+                    offset = template_center - zoomed_matched_center + 0.5
+                    offset = offset.astype(int)
+
+                    div = utils.get_div(template_am, zoomed_matched_am, offset)
+                    if best_div > div:
+                        best_div = div
+                        best_zoom_rate[0] = zoom_rate0
+                        best_zoom_rate[1] = zoom_rate1
+                        best_zoom_rate[2] = zoom_rate2
+
+        print(best_zoom_rate, best_div)
+
+        zoomed_matched_am = ndimage.zoom(matched_am, [best_zoom_rate[0], best_zoom_rate[1], best_zoom_rate[2]])
+        zoomed_matched_data = ndimage.zoom(matched_data, [best_zoom_rate[0], best_zoom_rate[1], best_zoom_rate[2]])
+
+        sub_block_id = block_id * 8 - 1
+
+        for block_anchor0 in self._block_anchors:
+
+            sub_template_anchor0 = block_anchor0 * template_am.shape[0]
+            sub_matched_anchor0 = block_anchor0 * matched_am.shape[0]
+
+            for block_anchor1 in self._block_anchors:
+
+                sub_template_anchor1 = block_anchor1 * template_am.shape[1]
+                sub_matched_anchor1 = block_anchor1 * matched_am.shape[1]
+
+                for block_anchor2 in self._block_anchors:
+
+                    sub_template_anchor2 = block_anchor2 * template_am.shape[2]
+                    sub_matched_anchor2 = block_anchor2 * matched_am.shape[2]
+
+                    sub_template_data = template_data[
+                                            sub_template_anchor0[0]:sub_template_anchor0[1],
+                                            sub_template_anchor1[0]:sub_template_anchor1[1],
+                                            sub_template_anchor2[0]:sub_template_anchor2[1]]
+
+                    sub_template_am = template_am[
+                                            sub_template_anchor0[0]:sub_template_anchor0[1],
+                                            sub_template_anchor1[0]:sub_template_anchor1[1],
+                                            sub_template_anchor2[0]:sub_template_anchor2[1]]
+                    
+                    sub_matched_data = zoomed_matched_data[
+                                            sub_matched_anchor0[0]:sub_matched_anchor0[1],
+                                            sub_matched_anchor1[0]:sub_matched_anchor1[1],
+                                            sub_matched_anchor2[0]:sub_matched_anchor2[1]]
+
+                    sub_matched_am = zoomed_matched_am[
+                                            sub_matched_anchor0[0]:sub_matched_anchor0[1],
+                                            sub_matched_anchor1[0]:sub_matched_anchor1[1],
+                                            sub_matched_anchor2[0]:sub_matched_anchor2[1]]
+
+                    sub_block_id += 1
+
+                    self._induce(sub_block_id, sub_template_data, sub_template_am, sub_matched_data, sub_matched_am)
+
+
         pass
 
     def validate(self):
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
+from scipy import ndimage
 import numpy as np
 
 
@@ -67,40 +68,148 @@ def check_outside(point, shape):
     return False
 
 
+def threshold(a, threshmin=None, threshmax=None, newval=0):
+    """
+    Clip array to a given value.
+    Similar to numpy.clip(), except that values less than `threshmin` or
+    greater than `threshmax` are replaced by `newval`, instead of by
+    `threshmin` and `threshmax` respectively.
+    Parameters
+    ----------
+    a : ndarray
+        Input data
+    threshmin : {None, float}, optional
+        Lower threshold. If None, set to the minimum value.
+    threshmax : {None, float}, optional
+        Upper threshold. If None, set to the maximum value.
+    newval : {0, float}, optional
+        Value outside the thresholds.
+    Returns
+    -------
+    threshold : ndarray
+        Returns `a`, with values less then `threshmin` and values greater
+        `threshmax` replaced with `newval`.
+    """
+    a_mask = np.ma.array(a, copy=True)
+    mask = np.zeros(a.shape, dtype=bool)
+    if threshmin is not None:
+        mask |= (a_mask < threshmin).filled(False)
+    if threshmax is not None:
+        mask |= (a_mask > threshmax).filled(False)
+    a[mask] = newval
+    return a
+
+
 def get_attention_map(img):
 
-    ways = []
-    for i in range(-1, 2):
-        for j in range(-1, 2):
-            for k in range(-1, 2):
-                ways.append((i,j,k))
+    # ways = []
+    # for i in range(-1, 2):
+    #     for j in range(-1, 2):
+    #         for k in range(-1, 2):
+    #             ways.append((i,j,k))
 
-    attention_map = np.zeros(img.shape)
+    # for i in range(img.shape[0]):
+    #     print(i)
+    #     for j in range(img.shape[1]):
+    #         for k in range(img.shape[2]):
+    #
+    #             cur_point = (i, j, k)
+    #             cur_intensity = img[cur_point]
+    #             sum_attention = 0
+    #             sum_neighbors = 0
+    #
+    #             for way in ways:
+    #
+    #                 neighbor_point = tuple([cur_point[d] + way[d] for d in range(3)])
+    #                 if check_outside(neighbor_point, img.shape):
+    #                     continue
+    #                 neighbor_intensiry = img[neighbor_point]
+    #                 sum_attention += (cur_intensity - neighbor_intensiry) ** 2
+    #                 sum_neighbors += 1
+    #
+    #             attention_map[cur_point] = sum_attention / (sum_neighbors - 1)
 
-    print(img.shape)
+    kernel = np.array([[[-1, 0, 1],
+                        [-2, 0, 2],
+                        [-1, 0, 1]],
 
-    for i in range(img.shape[0]):
-        print(i)
-        for j in range(img.shape[1]):
-            for k in range(img.shape[2]):
+                       [[-2, 0, 2],
+                        [-4, 0, 4],
+                        [-2, 0, 2]],
 
-                cur_point = (i, j, k)
-                cur_intensity = img[cur_point]
-                sum_attention = 0
-                sum_neighbors = 0
+                       [[-1, 0, 1],
+                        [-2, 0, 2],
+                        [-1, 0, 1]]])
+    sobel_map0 = ndimage.convolve(img, kernel)
 
-                for way in ways:
+    kernel = np.array([[[-1, -2, -1],
+                        [-2, -4, -2],
+                        [-1, -2, -1]],
 
-                    neighbor_point = tuple([cur_point[d] + way[d] for d in range(3)])
-                    if check_outside(neighbor_point, img.shape):
-                        continue
-                    neighbor_intensiry = img[neighbor_point]
-                    sum_attention += (cur_intensity - neighbor_intensiry) ** 2
-                    sum_neighbors += 1
+                       [[0, 0, 0],
+                        [0, 0, 0],
+                        [0, 0, 0]],
 
-                attention_map[cur_point] = sum_attention / (sum_neighbors - 1)
+                       [[1, 2, 1],
+                        [2, 4, 2],
+                        [1, 2, 1]]])
+    sobel_map1 = ndimage.convolve(img, kernel)
+
+    kernel = np.array([[[-1, -2, -1],
+                        [0, 0, 0],
+                        [1, 2, 1]],
+
+                       [[-2, -4, -2],
+                        [0, 0, 0],
+                        [2, 4, 2]],
+
+                       [[-1, -2, -1],
+                        [0, 0, 0],
+                        [1, 2, 1]]])
+    sobel_map2 = ndimage.convolve(img, kernel)
+
+    attention_map = (sobel_map0 ** 2 + sobel_map1 ** 2 + sobel_map2 ** 2) ** 0.5
+    attention_map = threshold(attention_map, 1e-6, newval=0)
 
     return attention_map
+
+
+def get_div(template_am, matched_am, offset):
+
+    template_anchor_st = np.array([0, 0, 0], dtype=int)
+    template_anchor_ed = np.array(template_am.shape, dtype=int)
+    matched_anchor_st = np.array([0, 0, 0], dtype=int) + offset
+    matched_anchor_ed = np.array(matched_am.shape, dtype=int) + offset
+
+    inter_anchor_st = np.zeros(3, dtype=int)
+    inter_anchor_ed = np.zeros(3, dtype=int)
+    cube_flag = True
+    for i in range(3):
+
+        inter_anchor_st[i] = max(template_anchor_st[i], matched_anchor_st[i])
+        inter_anchor_ed[i] = min(template_anchor_ed[i], matched_anchor_ed[i])
+
+        if inter_anchor_st[i] >= inter_anchor_ed[i]:
+            cube_flag = False
+
+    if cube_flag is False:
+        return -1
+
+    ROI_anchor_st = inter_anchor_st - offset
+    ROI_anchor_ed = inter_anchor_ed - offset
+
+    matched_am_inter = np.zeros(template_am.shape)
+    matched_am_inter[
+        inter_anchor_st[0]:inter_anchor_ed[0],
+        inter_anchor_st[1]:inter_anchor_ed[1],
+        inter_anchor_st[2]:inter_anchor_ed[2]] = matched_am[
+                                                    ROI_anchor_st[0]:ROI_anchor_ed[0],
+                                                    ROI_anchor_st[1]:ROI_anchor_ed[1],
+                                                    ROI_anchor_st[2]:ROI_anchor_ed[2]]
+
+    div = (abs(template_am - matched_am_inter)).mean()
+
+    return div
 
 
 if __name__ == '__main__':
